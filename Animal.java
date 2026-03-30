4300648e9125406a35eaff57a706a31e068a9fa2
barasevna sasha <barashevnasa@gmail.com>
2026-03-30 17:24:25 +0300

    домашнее задание №9

---
+++ Animal.java
@@ -0,0 +1,11 @@
+//@author Aliaksandra_Baranava
+
+public abstract class Animal {
+    public void voice(){
+        System.out.println("Звуки животного.");
+    }
+
+    public void eat(String food){
+        System.out.println("Животное ест.");
+    }
+}
---
+++ FoodOptions.java
@@ -0,0 +1,10 @@
+//@author Aliaksandra_Baranava
+
+public class FoodOptions {
+    public static final String MEAT = "Мясо";
+    public static final String FISH = "Рыба";
+    public static final String GRASS = "Трава";
+    public static final String CARROT = "Морковь";
+    public static final String CEREAL = "Крупа";
+    public static final String SWEET = "Сладкое";
+}
